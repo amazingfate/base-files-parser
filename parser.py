ae4e2fed0d6bebfe6fa9a72c9acf1d7fb64abc7a
amazingfate <liujianfeng1994@gmail.com>
2024-11-14 13:05:03 +0800

    make script more common

--- parser.py
+++ parser.py
@@ -2,10 +2,18 @@
 import requests
 from lxml import etree
 import json
+import sys
 
-def get_basefiles_info_from_upstream(debian_url):
+def get_package_info_from_upstream(distro, pacakge_name):
+    if distro == 'debian':
+        distro_url = "https://packages.debian.org/search?keywords=" + pacakge_name + "&searchon=names&suite=all&section=all"
+    elif distro == 'ubuntu':
+        distro_url = "https://packages.ubuntu.com/search?keywords=" + pacakge_name + "&searchon=names&suite=all&section=all"
+    else:
+        print("invalid distro %s, quit" % distro)
+        sys.exit(1)
     # Step 1: Fetch HTML content from the URL
-    response = requests.get(debian_url)
+    response = requests.get(distro_url)
     html_content = response.content  # Use .content for lxml to handle byte data
 
     # Step 2: Parse HTML with lxml
@@ -17,37 +25,43 @@ def get_basefiles_info_from_upstream(debian_url):
         section_title = h3.text
 
         ul = h3.xpath('./following-sibling::ul[1]')
-        debian_all_basefiles_info = {}
+        debian_all_package_info = {}
         if ul:
             list_items = ul[0].xpath('.//li')
             for li in list_items:
-                debian_basefiles_info = {}
+                debian_package_info = {}
                 item_text = li.xpath('.//text()[not(parent::a)]')
                 item_class = li.get("class")
-                base_file_release = item_class
-                base_file_version = item_text[1].split(":")[0]
+                package_file_release = item_class
+                package_file_version = item_text[1].split(":")[0]
                 if "arm64" in item_text[1].split(":")[1]:
-                    base_file_arm64_full_name = "/pool/main/b/base-files/base-files_" + base_file_version + "_arm64.deb"
-                    debian_basefiles_info["arm64"] = base_file_arm64_full_name
+                    package_file_arm64_full_name = "/pool/main/b/base-files/base-files_" + package_file_version + "_arm64.deb"
+                    debian_package_info["arm64"] = package_file_arm64_full_name
                 if "armhf" in item_text[1].split(":")[1]:
-                    base_file_armhf_full_name = "/pool/main/b/base-files/base-files_" + base_file_version + "_armhf.deb"
-                    debian_basefiles_info["armhf"] = base_file_armhf_full_name
+                    package_file_armhf_full_name = "/pool/main/b/base-files/base-files_" + package_file_version + "_armhf.deb"
+                    debian_package_info["armhf"] = package_file_armhf_full_name
                 if "amd64" in item_text[1].split(":")[1]:
-                    base_file_amd64_full_name = "/pool/main/b/base-files/base-files_" + base_file_version + "_amd64.deb"
-                    debian_basefiles_info["amd64"] = base_file_amd64_full_name
+                    package_file_amd64_full_name = "/pool/main/b/base-files/base-files_" + package_file_version + "_amd64.deb"
+                    debian_package_info["amd64"] = package_file_amd64_full_name
                 if "riscv64" in item_text[1].split(":")[1]:
-                    base_file_riscv64_full_name = "/pool/main/b/base-files/base-files_" + base_file_version + "_riscv64.deb"
-                    debian_basefiles_info["riscv64"] = base_file_riscv64_full_name
-                debian_all_basefiles_info[item_class] = debian_basefiles_info
-        return debian_all_basefiles_info
+                    package_file_riscv64_full_name = "/pool/main/b/base-files/base-files_" + package_file_version + "_riscv64.deb"
+                    debian_package_info["riscv64"] = package_file_riscv64_full_name
+                debian_all_package_info[item_class] = debian_package_info
+        return debian_all_package_info
 
-debian_url = "https://packages.debian.org/search?keywords=base-file&searchon=names&suite=all&section=all"
-debian_info = get_basefiles_info_from_upstream(debian_url)
-ubuntu_url = "https://packages.ubuntu.com/search?keywords=base-file&searchon=names&suite=all&section=all"
-ubuntu_info = get_basefiles_info_from_upstream(ubuntu_url)
-if debian_url and debian_url:
+if len(sys.argv) < 2:
+    print("Usage: python parse.py <pacakge_name>")
+    sys.exit(1)
+
+package_name = sys.argv[1]
+
+debian_info = get_package_info_from_upstream("debian", package_name)
+ubuntu_info = get_package_info_from_upstream("ubuntu", package_name)
+if debian_info and ubuntu_info:
     all_info_result = {**debian_info, **ubuntu_info}
-    with open("result.json", "w") as outfile:
+    json_file_name = package_name + ".json"
+    with open(json_file_name, "w") as outfile:
         json.dump(all_info_result, outfile)
 else:
-    print("failed")
+    print("failed to get package info")
+    sys.exit(1)
